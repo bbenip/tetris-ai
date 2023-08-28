67964bf1bc3c8215755747943773a603c03b7664
Marquelle Jones <jonesmarquelle@github.com>
2023-08-28 13:40:03 -0400

    Fix duplicate replay bug

--- replayer/openReplay.py
+++ replayer/openReplay.py
@@ -1,6 +1,7 @@
 import os
 import time
 import json
+import random
 
 from requests import get
 
@@ -26,13 +27,14 @@ NUMBERED_REPLAY_FORMAT = '%s.json'
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 REPLAY_TEXT = 'N4IgxiBcoG5QzAGhAZwPYDMAuARATmgA4CSAJlACzIDmAhgLYCmAylrXllAIwCsATAE4BAdgEVhwgAzweNBowCiAO3KRegkWIHwhk5CkaNVIAF4CT9UmhDJ63ZACMUUPakYvkpWs8gA2YQC+nlAgAEIUYADyPAAeAGIAinGMxABaAGoxhDEAEgDSmAkAngoAgsQA1GAANmAAltRxADIA1mAA7tQAKjmSkZEKvjwmFAAWCXgAcnEoCgAaAKoASvCkALKlCpItCwoAUgDqk1wAju1zAJqkGCgHCV2pfEuEoXxoSw5Kk+0oOcxzpDwcXghDmeDmGAWAGEoQk0Ok5qMABzMSZYCjwJYAKwEB2YFB4Fzm9FozFoaD2vhyaDwAHF6DxfNVSiclMwAK6lPB7OZYDDMYRcUSlGBcLo4CgoVJLIqYSJYUhNOZcBY4dKpSb0IrwOIYUhgFJdHjMLrVGJVBK+eBNLAKDA8gTEHJSDDwHKlOabJbtRihKECNaRUqlCrwO0KO4xegLWlcMCkFBQprMPYOMBKY0YUZNSQKGLpExdFqTQgKHhYEySagJah7faSDAKZh5UJrUaSPijLq+dLMNBxuYHBZxUjpLgVUKpWmTapI2kYHK0FpLLDEA4VBZgBakExgObULGMDChYRYeCSBtKPI4OIUBKjUYmA7ESQUPg5FqhZjMUqp+D0OIoVSUY9iKOo0EmZg6lGXwlByPAFChGIWjwBF2R4RgAHokTABITBieAcDyJYkSKUITDQOYEhQARIngLgFwcYgBD4Fp4CxDA6gNRh4F8NYcnRTDSjiJFSgABRMSZMNoahQyaAR2gSHAlFKEpiAUdoTkkHBaTAMTqiWMScjiH9Unads3QwYhRmoMkr2DFAD2IeBIjmMAkRQLo+C4JZQjiYzJDqUoukmPYxJaXt4AOPUwDXKEuguPh0noapWNKWgliWdliCUUJJDAUZSEkNY5nSZJfDiARqgoBQWlGHAoRyPYxlCLEDkiBIeD2Wk5mqJohylNAchU2lSDiWgFAovhGCxNA8lGIo8FIRh0QcJosSwHIoS4PrRjmdkkShCc1lpeAYkmOY9lCJFOUkUomlNARjz4PlSFKRgchwHhSC6JRRgqdoFmEerJFIMTqHgCpqhyJFrmELFSiwMAFGIUoFpwHAohQUI8hyBRhAUC48zQahqRrUC9hAwg1iBKF4DmNYEgULg1hMOo+k3Cou18cYjAUJEFl65hqEmJo4kkXxqAWPhDiwLgSbmGA9noR5AbAGBSB4JZepsvJaRwGIKAuQKGrWZqeHaFoYDyA5Rh4WhRnoLhIlHPYxTQBJGBaNZaAOFpWUYFBqloIoYhQZbJAWJp6ChGBovCNYDngIoFzyHgwB4LoOzmLoUEmdIlD4NYmlIcdSnCMS5m9qpeLmHJ2TQCphHaJEHAwSSxLE5gKkILphD2do6NCGAhqRCYuBwSI1kymJmBOWk1gUBxJihUolj2KEwAAXhAAIgA'
 REPLAY_TEXT_2 = "N4IgxiBcoG5QzAOngGhAZwPYDMAuARAJ0wAcBJAEwTQHMBDAWwFMBlXOw3KARgDYBOAEwAGAOwBWeON4AWbgA5ajJgFEAdlUh8hY8XOHDeotOiZNNIEnXQBLAB4ArEGgZRBaAEboowk0zdoFNY88KggHIQ8viCRkMIAvoFQIACCKTQpAMIAivjiKdgA1vg52QBSAEIshQDiANIAStwyZQxgopgqAOoArmTcYA2iMgCOFZmYYCMkAJ6EZXU96ISZM+i4dQboANQA7vLC8gAKKTMAmg5HLAByZyoAagA2ACpd8NcUYNzbz2ce1w50IIABrPbCiGoMZ78Gh2QQAMSOdAAqvw7AAveAkI40QrbOrogD0AAsUo8HA58DZsrwSCoYDceoUHPxstkKmV5CpdmVkfIKjQWDV8AAtM4UR42XhdbIikVHR4MTLA+GYYnIpj3eRlQrcNTCcTlGaCTIAGTOJFw6IYMhYHkeJCZDFEXTANDIghI2HQ2SO-X4IvEomBmPRNCOhQq+BmKjsZHuAFliWAZjV7mQPGAHI9sl0mLwyDZ0HYWKIVAmKmQ7AB5BzVo4qXjodEzdFHXDXFIHU3oi3Chy8CmEeQ9UQwbDIs78MBqQSCbDYEjbatqcTbbCEcQVR70e4zEY0bIOYmZXjIo7iBrZR41TLIsAkFiZR4sGw1YkabYVbbiM7wnrZM8bpSPC2ToGodDCPCzz3CqWrcDQrwONcYAJtw4hATeTDovwZR2MIuBHHy07PIIDRHB4CaEtguw1NsMgyCk+DbCMLD8LsDAktkAASmQwPC1ZnDQ4gjM8IziNWoiFPIgjPPAIrPO+FDwoIMDJiQ9zcEQ6DccCYBMDAwimmQIwqOghTCHYDRgtc2RfAmMH4NcXQJvAPQNI8dTbISdS4XUKSkvgCY0PcLAVPAZBlG6PQsGUMAVAm+CPLwdRgDYdSfGU-AsJgwJ1A4UxdCwMCEvgmCmjAYAVHUhJmcIMyiOiCYNAmTXCPAdhgLw2zZGcdBlKamDiOiLDWmoMDVmAMjiOIaiYCMTAMCQvCEGo6AMCkuU1HcDjceQVzEtwdTIrwxKEI0ogkAm2XLY86LcAmhAjLs1jPHU8CCKIC7AioTDuZ8-D1MSMjoD0djEhQDiltxZSCDY-C8LsZ2EMCZCUM8wIyMG4gitkdBRvchTZMSdgePC3FnCkNr4HU3HVjYKjbMS9y2cCNQyDA8C-Ck1bZHUvC4HC2AOD0hJkEc8CFLwIwUBUIoyA0hDwjUoh1B4djZHYNjYEcYDVu9tWENsrKPPCoopHQ9xwiMZAVCoJBqBpVw9D0mB1LivC8O6KOrIULqYGD+ApDUCb3KIMwig42Q0DCZBnLgFQMLsPRMNxnbVh4prPLg8gTV5yLMWAZANKaJMpKahK7CK+A0MIRyZCuZRkLadgcWoNisIQIojD0wJZwHPSZISwgJl0MgzGozzXOg-CVtwHgOHQy4pM8MjwBUmDcei1nOsOuTol0MBdGoTB2Ew-AjPw2zyOC6KYJkKQVHyp4yGmuxMGALB0Gn8AqPIIrIkyNsTA1YKDZFwLwQodARiPEyCMDw-BgQkAoJeW0uwbAumuDAQoFA1BkEJKwGo8g1DYCYOIBgOldidT7qaBgZxsAJmENsEUMwvgig0IUD+8AL4pG4uxOwvAVAsGRGTFgZAaAJjsvABMyIahVHEPCWEghrh1BFKIeEhBHhnEqDYfATBrg9DpB5boyDRDcWJD0IwTBgSCH4DAXA2xrjAmNrsSSMd4AwzAGAbYvBHjEl2I8I41xCD8BsNwQQ39iQjG2DAeQdjxD8l2Ko3YhAejMRsM8Hov5rjwnxJBb82j-wMDUCwYGxk6joEJKTKkMhuI1BGJ1QkntDqCG4qk4EwIaA9BgFlGgZwGAqDAGUFQ3FTYjBVCKOwNASDiEyHYQopo1DcXQdxZ4NBri7HgDICghBCgyBXGHdAkhGqbLULsCuXYfjiH4FvbYYBsjwnhN+QRFBhBlEJJgLo3Fsjv2JJgQGhQwApAALwgHiEAA"
-REPLAY_ID = 1234
+REPLAY_ID = random.randint(1000, 9999)
 
 LOAD_BUTTON_ID = 'load'
 PAUSE_BUTTON_ID = 'pause'
 REPLAY_TEXT_ID = 'rep0'
 DOWNLOAD_BTN_ID = 'fullDL'
 
+# Load games from JSON file
 def loadGames(filepath):
     out = []
     with open(filepath, 'rb') as f:
@@ -41,7 +43,7 @@ def loadGames(filepath):
             out.append(game["d"])
     return out
 
-# Intercepts requests going to "some server"
+# Intercepts outgoing requests
 def interceptor(request):
     if REPLAYER_URL in request.url:
         print("requested replayer", request.url)
@@ -55,7 +57,7 @@ def interceptor(request):
 
 def setupDriver():
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless") # Uncomment this to run the replays without GUI
     chrome_options.add_argument(f'user-agent={USER_AGENT}')
 
     prefs = {}
@@ -66,9 +68,10 @@ def setupDriver():
     driver = webdriver.Chrome(options=chrome_options)
 
     driver.request_interceptor = interceptor
-    driver.scopes = ['.*jezevec10.*']
+    driver.scopes = ['.*jezevec10.*'] # Limit proxying to sites with jezevec10
     return driver
 
+# Synchronously polls to see if a file has been created
 def wait_for_file(file_path, timeout):
     time_counter = 0
     while not os.path.exists(file_path):
@@ -82,12 +85,13 @@ def wait_for_file(file_path, timeout):
 
 def runReplay(driver, replay, replayID):
     replayTextArea = driver.find_element(By.ID, REPLAY_TEXT_ID)
+    replayTextArea.clear()
     replayTextArea.send_keys(replay)
     loadButton = driver.find_element(By.ID, LOAD_BUTTON_ID)
     loadButton.click()
 
+    # Wait for modified replayer to create a json download link for us to click
     wait = WebDriverWait(driver, 10, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
-
     downloadButton = WebDriverWait(driver, 60).until(
         EC.presence_of_element_located((By.ID, DOWNLOAD_BTN_ID))
     )
@@ -95,6 +99,7 @@ def runReplay(driver, replay, replayID):
     pauseButton = driver.find_element(By.ID, PAUSE_BUTTON_ID)
     pauseButton.click()
 
+    # Download link gives a file named "replay.json" every time, so we rename it
     url = downloadButton.get_attribute('href')
     default_file_path = os.path.join(DOWNLOAD_FOLDER_PATH, DEFAULT_REPLAY_FILENAME)
     downloadButton.click()
@@ -127,5 +132,5 @@ while input() != "close":
 # except:
 #     print("Error running replay scraper")
 # finally:
-    tearDownDriver(driver)
+tearDownDriver(driver)
 
